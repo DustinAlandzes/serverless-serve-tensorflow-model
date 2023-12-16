3e9a66a37660bfecd38a9e40ddb0c96f5cc43be7
Dustin Alandzes <fezf00@gmail.com>
2023-12-16 15:16:02 -0600

    run terraform fmt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,6 @@ output "serverless_module_s3_bucket" {
 }
 
 output "serverless_module_s3_bucket_url" {
-    description = "The URL of the S3 bucket"
-    value       = "https://${aws_s3_bucket.serverless_module.bucket.id}.s3.${aws_s3_bucket.serverless_module.bucket.region}.amazonaws.com/index.html"
+  description = "The URL of the S3 bucket"
+  value       = "https://${aws_s3_bucket.serverless_module.bucket.id}.s3.${aws_s3_bucket.serverless_module.bucket.region}.amazonaws.com/index.html"
 }
