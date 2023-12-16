1ce6bd7a147eaf8ee3a7a61f8ef2900b4b3018b5
Dustin Alandzes <fezf00@gmail.com>
2023-12-16 14:45:41 -0600

    output s3 bucket name and url

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "serverless_module_s3_bucket" {
+  value       = aws_s3_bucket.serverless_module.bucket
+  description = "The S3 bucket's name"
+}
+
+output "serverless_module_s3_bucket_url" {
+    description = "The URL of the S3 bucket"
+    value       = "https://${aws_s3_bucket.serverless_module.bucket.id}.s3.${aws_s3_bucket.serverless_module.bucket.region}.amazonaws.com/index.html"
+}
