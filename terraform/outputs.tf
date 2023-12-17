9c665015957e76db4e884c17512c0933b014667e
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2023-12-16 20:35:39 -0600

    fix deploy (#11)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,14 @@ output "serverless_module_s3_bucket" {
   value       = aws_s3_bucket.serverless_module.bucket
   description = "The S3 bucket's name"
 }
+
+output "aws_key" {
+  value       = aws_iam_access_key.serverless_module.id
+  description = "AWS key for IAM user"
+}
+
+output "aws_secret" {
+  value       = aws_iam_access_key.serverless_module.secret
+  description = "AWS secret for IAM user"
+  sensitive   = true
+}
