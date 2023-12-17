dd5eef12c7d42bf03c507439566295b57c0a7cdc
Dustin Alandzes <fezf00@gmail.com>
2023-12-17 15:19:20 -0600

    regenerate chalice config, fix deployment script

--- backend/chalice_project/generate-terraform-files.sh
+++ backend/chalice_project/generate-terraform-files.sh
@@ -1,7 +1,7 @@
 export AWS_DEFAULT_REGION=us-west-2
-chalice package --pkg-format terraform ../terraform
+chalice package --pkg-format terraform ../../terraform/chalice
 
 # remove the provider block
-cat ../terraform/chalice.tf.json | jq 'del(.terraform)' > /tmp/chalice.tf.json
-rm ../terraform/chalice.tf.json
-mv /tmp/chalice.tf.json ../terraform/chalice.tf.json
+cat ../../terraform/chalice/chalice.tf.json | jq 'del(.terraform)' > /tmp/chalice.tf.json
+rm ../../terraform/chalice/chalice.tf.json
+mv /tmp/chalice.tf.json ../../terraform/chalice/chalice.tf.json
