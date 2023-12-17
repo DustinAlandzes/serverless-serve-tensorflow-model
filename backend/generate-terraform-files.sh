717e55c591c58a660c1408dde709bab105ba81ca
Dustin Alandzes <fezf00@gmail.com>
2023-12-17 14:50:01 -0600

    remove the provider block

--- backend/generate-terraform-files.sh
+++ backend/generate-terraform-files.sh
@@ -1,2 +1,7 @@
 export AWS_DEFAULT_REGION=us-west-2
 chalice package --pkg-format terraform ../terraform
+
+# remove the provider block
+cat ../terraform/chalice.tf.json | jq 'del(.terraform)' > /tmp/chalice.tf.json
+rm ../terraform/chalice.tf.json
+mv /tmp/chalice.tf.json ../terraform/chalice.tf.json
