486cb9f500368858871d70cd7c410fb9af14c4f6
Dustin Alandzes <fezf00@gmail.com>
2023-12-17 21:31:42 -0600

    type error

--- frontend/src/pages/AnotherPage.tsx
+++ frontend/src/pages/AnotherPage.tsx
@@ -1,4 +1,5 @@
 import {gql, useQuery} from "@apollo/client";
+import {Book} from "../__generated__/graphql.ts";
 
 const GET_BOOKS = gql(`
 query GetBooks {
@@ -11,7 +12,7 @@ query GetBooks {
 function BooksList() {
     const {loading, data} = useQuery(GET_BOOKS)
     return <>{loading ? (<p>{"Loading"}</p>) :
-        (data && data.books.map(book => (
+        (data && data.books.map((book: Book) => (
             <>{book.title} - {book.author}</>
         )))
     }</>
