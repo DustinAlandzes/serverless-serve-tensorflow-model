481502a9988ab323090ea1f0db89c16c420526d9
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2023-12-18 23:51:18 -0600

    add cloudfront distribution in front of s3 bucket for https (#25)

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,55 @@ resource "aws_s3_bucket_website_configuration" "serverless_module" {
     key = "index.html"
   }
 }
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_bucket_cloudfront_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.serverless_module.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "POST"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
 resource "aws_iam_user" "serverless_module" {
   name = "serverless-module-s3-bucket-user"
 }
