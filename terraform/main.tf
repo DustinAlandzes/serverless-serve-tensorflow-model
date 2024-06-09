09820144b30f9cfdcbc3286f45f8b41fbf967d6f
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2024-06-09 16:11:18 -0500

    Fourth try (#4)
    * Make some small adjustments and try to deploy
    
    * update terraform version and remove parser options from eslint config
    
    * try to make cloudfront distribution work with react-router routes, try using secret instead of hardcoded string for VITE_BACKEND_GRAPHQL_ENDPOINT, disable cors

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,6 +92,20 @@ resource "aws_cloudfront_distribution" "s3_bucket_cloudfront_distribution" {
 
   price_class = "PriceClass_100"
 
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
